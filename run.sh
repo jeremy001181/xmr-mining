b18222ae85c0e5a23c03219451364a1ed290c32d
jeremy001181 <jeremy001181@gmail.com>
2018-02-24 18:10:39 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 #make sure a process is always running.
 
 process=minerd
-makerun="sudo ../cpuminer-multi/minerd -a cryptonight -o stratum+tcp://xmr.bohemianpool.com:4444 -u 45Szo5vQ2rtV68GPqgi9UQ1dg357jwVd2LoWZYn9uY6kFdrSJDcrWfaXLVBBTTCtbrWdB2azjgS1HadpDenghciv3ezAdp7 -p $(hostname) -t 3"
+makerun="sudo ../cpuminer-multi/minerd -a cryptonight -o stratum+tcp://gulf.moneroocean.stream:10001 -u 45Szo5vQ2rtV68GPqgi9UQ1dg357jwVd2LoWZYn9uY6kFdrSJDcrWfaXLVBBTTCtbrWdB2azjgS1HadpDenghciv3ezAdp7 -p $(hostname) -t 3"
 
 if ps ax | grep -v grep | grep $process > /dev/null
 then
